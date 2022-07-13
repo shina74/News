aa5e8821503e4f5207240111fe89183c168228b6
shina74 <103118554+shina74@users.noreply.github.com>
2022-07-13 14:13:01 +0500

    Delete NewsPaper directory

--- NewsPaper/news/filters.py
+++
@@ -1,8 +0,0 @@
-from django_filters import FilterSet
-from .models import Post,Author,User
-
-
-class PostFilter(FilterSet):
-    class Meta:
-        model = Post
-        fields = ('author', 'head', 'time_create')
--- NewsPaper/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# Импортируем созданное нами представление
-from .views import PostList, PostView
-
-
-urlpatterns = [
-   path('', PostList.as_view()),
-   path('search', PostView.as_view()),
-]
